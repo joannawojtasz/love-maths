8949004dee36e15dff581641460d8e2eef10c26d
joannawojtasz <joannawojtasz@gmail.com>
2021-06-27 22:41:52 +0000

    Set up structure of games functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+ function runGame () {
+
+ }
+
+ function checkAnswer() {
+
+ }
+
+ function calculateCorrectAnswer() {
+
+ }
+
+ function incrementScore() {
+
+ }
+
+ function incrementWrongAnswer() {
+
+ }
+
+ function displatAdditionQuestion() {
+
+ }
+
+ function displaySubtractQuestion() {
+
+ }
+
+ function displayMultiplyQuestion() {
+
+ }
+
+ function displayDivideQuestion() {
+     
+ }
