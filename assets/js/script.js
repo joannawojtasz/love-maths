5f0f77260dfe572b1f1ae4dff5d9fb71e7da89d5
joannawojtasz <joannawojtasz@gmail.com>
2021-06-28 00:34:49 +0000

    Add code to increment scoew and incorrect anwers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,11 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
     if (isCorrect) {
         alert("Hey! You got it right! :)");
+        incrementScore();
+
     } else {
         alert(`Awww..... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`)
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1])
@@ -72,12 +75,20 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').textContent);
+    document.getElementById('score').textContent =  ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').textContent);
+    document.getElementById('incorrect').textContent =  ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
